32a7a53c22461b4b6869ed6236d6679a37f9d5b1
Siddarth Iyer <mindstormer619@gmail.com>
2022-12-02 00:07:31 +0530

    Day 1

---
+++ src/day01/mod.rs
@@ -0,0 +1,34 @@
+use crate::utils::linesFromFile;
+
+pub fn solve(input: &str) {
+    let fileContent = linesFromFile(format!("src/day01/{}.txt", input));
+
+    let mut currentWeight = 0;
+    let mut elves: Vec<u32> = Vec::new();
+
+    for line in fileContent {
+        if line == "" {
+            elves.push(currentWeight);
+            currentWeight = 0;
+        } else {
+            currentWeight += line.parse::<u32>().expect("fuck off")
+        }
+    }
+
+    elves.sort();
+
+    match heaviestElf(&elves) {
+        Some(elf) => println!("{elf}"),
+        None => println!("fuck off"),
+    }
+
+    println!("{:?}", heaviest3Elves(&elves))
+}
+
+fn heaviestElf(elves: &Vec<u32>) -> Option<&u32> {
+    elves.last()
+}
+
+fn heaviest3Elves(elves: &Vec<u32>) -> u32 {
+    (&elves[elves.len() - 3..]).iter().sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#![allow(non_snake_case)]
+
+use crate::day01::solve;
+
+pub mod day01;
+
+pub mod utils {
+    use std::{
+        fs::File,
+        io::{BufRead, BufReader},
+        path::Path,
+    };
+
+    pub fn linesFromFile(filename: impl AsRef<Path>) -> Vec<String> {
+        let file = File::open(filename).expect("No fucking way");
+        let buf = BufReader::new(file);
+
+        buf.lines()
+            .map(|l| l.expect("Could not parse line"))
+            .collect()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, elves!");
+
+    solve("test");
+    solve("prod");
 }
